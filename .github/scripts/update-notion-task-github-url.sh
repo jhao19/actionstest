4ad39448501afc95a27bec716b6de98fc81925c1
Jonathan Hao <jonathan@healthrhythms.com>
2022-11-10 16:57:38 -0500

    add debug 1

--- .github/scripts/update-notion-task-github-url.sh
+++
@@ -1,9 +0,0 @@
-curl --location -X PATCH "https://api.notion.com/v1/pages/$1" \
-    --header "Notion-Version: 2022-06-28" \
-    --header "Content-Type: application/json" \
-    --header "Authorization: Bearer $2" \
-    --data-raw "{
-        \"properties\": {
-            \"GitHub URL\": { \"url\": \"$3\" }
-        }
-    }"
